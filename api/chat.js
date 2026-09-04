0f6e252b4d4e438789ca3e797032e0b21925d9d2
5ul01 <sulonpostia@gmail.com>
2026-09-05 00:31:59 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         "X-Title": "Dalbayob AI"
       },
       body: JSON.stringify({
-        model: "google/gemma-4-31b-it:free",
+        model: "inclusionai/ling-3.0-tiny:free",
         temperature: 1.0,
         max_tokens: 2000,
         messages: [
